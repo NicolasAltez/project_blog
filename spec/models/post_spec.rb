9da9f3b9c4f8ac490d0957c426e9d4bfe2a7cafd
nicolasAltez <nicolas.altez@these.com.uy>
2023-06-02 00:41:53 -0300

    refactor model post test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe Post, type: :model do
         should validate_presence_of(:title)
         should validate_presence_of(:content)
     end
+
+    it "triggers after_create_commit callback" do
+        post = Post.new(title: "titulo", content: "contenido")
+        allow_any_instance_of(Post).to receive(:broadcast_prepend_to).with('posts')
+        post.save
+    end
+    
+    it "triggers after_update_commit callback" do
+        post = Post.create(title: "titulo", content: "contenido")
+        allow_any_instance_of(Post).to receive(:broadcast_replace_to).with('posts')
+        post.update(title: "Updated Post")
+    end
+    
+    it "triggers after_destroy_commit callback" do
+        post = Post.create(title: "titulo", content: "contenido")
+        allow_any_instance_of(Post).to receive(:broadcast_remove_to).with('posts')
+        post.destroy
+    end
 end
