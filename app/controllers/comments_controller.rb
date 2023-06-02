71eca70afce3c7e1a6ea59c6e7016cb01fad0553
nicolasAltez <nicolas.altez@these.com.uy>
2023-06-02 02:41:29 -0300

    implemented comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,58 @@
+class CommentsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_post
+  
+    def create
+      @comment = @post.comments.build(comment_params)
+      @comment.user = current_user
+  
+      respond_to do |format|
+        if @comment.save
+          format.html { redirect_to post_path(@post)}
+          format.turbo_stream { render turbo_stream: turbo_stream.append(@comment) }
+        else
+          format.html { redirect_to post_path(@post)}
+          format.turbo_stream { render turbo_stream: turbo_stream.replace(@comment, partial: 'comments/form', locals: { comment: @comment }) }
+        end
+      end
+    end
+  
+    def edit
+      @comment = @post.comments.find(params[:id])
+    end
+  
+    def update
+      @comment = @post.comments.find(params[:id])
+  
+      respond_to do |format|
+        if @comment.update(comment_params)
+          format.html { redirect_to post_path(@post)}
+          format.turbo_stream { render turbo_stream: turbo_stream.update(@comment) }
+        else
+          format.html { render :edit }
+          format.turbo_stream { render turbo_stream: turbo_stream.replace(@comment, partial: 'comments/form', locals: { comment: @comment }) }
+        end
+      end
+    end
+  
+    def destroy
+      @comment = @post.comments.find(params[:id])
+      @comment.destroy
+  
+      respond_to do |format|
+        format.html { redirect_to post_path(@post)}
+        format.turbo_stream { render turbo_stream: turbo_stream.remove(@comment) }
+      end
+    end
+  
+    private
+  
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+  
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
+  end
+  
