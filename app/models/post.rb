a052515def307d9c58a0cf37f8dfb649d283898c
nicolasAltez <nicolas.altez@these.com.uy>
2023-06-02 00:41:30 -0300

    add broadcast to model post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_one_attached :image
     validates :title, :content, presence: true
+
+    after_create_commit { broadcast_prepend_to 'posts' }
+    after_update_commit { broadcast_replace_to 'posts' }
+    after_destroy_commit { broadcast_remove_to 'posts' }
 end
